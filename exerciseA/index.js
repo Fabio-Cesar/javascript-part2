450052e8ed6f3eb8b55657ff2ed3b856e5f08f7d
Fábio César <fabiocesar_33@outlook.com>
2022-06-09 13:38:23 -0300

    update exerciseA to use switch default case, replace ',' with '.' for the text input

--- exerciseA/index.js
+++ exerciseA/index.js
@@ -1,51 +1,49 @@
 function numberInWords() {
-    const number = parseFloat(document.querySelector('#number').value, 10);
+    const number = parseFloat(document.querySelector('#number').value.replace(',', '.'), 10);
     let result = '';
     document.querySelector('#result').innerText = result;
     if (isNaN(number)) {
         alert('Digite um número!');
     }
     else if (number % 1 === 0) {
-        if ((number >= 0) && (number <= 10)) {
-            switch(number) {
-                case 0:
-                    result += "Zero";
-                    break;
-                case 1:
-                    result += "Um";
-                    break;
-                case 2:
-                    result += "Dois";
-                    break;
-                case 3:
-                    result += "Três";
-                    break;
-                case 4:
-                    result += "Quatro";
-                    break;
-                case 5:
-                    result += "Cinco";
-                    break;
-                case 6:
-                    result += "Seis";
-                    break;
-                case 7:
-                    result += "Sete";
-                    break;
-                case 8:
-                    result += "Oito";
-                    break;
-                case 9:
-                    result += "Nove";
-                    break;
-                case 10:
-                    result += "Dez";
-            }
-            document.querySelector('#result').innerText = result;
+        switch(number) {
+            case 0:
+                result += "Zero";
+                break;
+            case 1:
+                result += "Um";
+                break;
+            case 2:
+                result += "Dois";
+                break;
+            case 3:
+                result += "Três";
+                break;
+            case 4:
+                result += "Quatro";
+                break;
+            case 5:
+                result += "Cinco";
+                break;
+            case 6:
+                result += "Seis";
+                break;
+            case 7:
+                result += "Sete";
+                break;
+            case 8:
+                result += "Oito";
+                break;
+            case 9:
+                result += "Nove";
+                break;
+            case 10:
+                result += "Dez";
+                break;
+            default:
+                alert('O número deve ser de 0 a 10!');
         }
-        else {
-            alert('O número deve ser de 0 a 10!');
-        }
+        document.querySelector('#result').innerText = result;
     }
     else {
         alert('O número deve ser inteiro!')
